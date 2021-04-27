0a4bcfb616d200226a0297788bd32a14f1e2c971
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-04-27 10:24:15 +0700

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::f64::consts::PI;
 use std::io::Empty;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -8,8 +7,6 @@ use piston_window::types::Color;
 use rand::{Rng, RngCore, thread_rng};
 use rand::rngs::ThreadRng;
 
-use crate::TileState::Snake;
-
 const WINDOW_TITLE: &str = "Rust Snake";
 const WINDOW_WIDTH_PIXELS: f64 = 640.0;
 const WINDOW_HEIGHT_PIXELS: f64 = 480.0;
@@ -18,7 +15,6 @@ const TILE_SIZE: f64 = 10.0;
 
 const ROW_COUNT: usize = WINDOW_HEIGHT_PIXELS as usize / TILE_SIZE as usize;
 const COL_COUNT: usize = WINDOW_WIDTH_PIXELS as usize / TILE_SIZE as usize;
-
 
 const COLOR_WHITE: Color = [200.0, 200.0, 200.0, 1.0];
 const COLOR_RED: Color = [200.0, 0.0, 0.0, 1.0];
@@ -30,28 +26,6 @@ const COLOR_SNAKE: Color = COLOR_RED;
 
 const FRAME_PER_SECONDS: u128 = 60;
 const MILLIS_PER_FRAME: u128 = (1000.0 / FRAME_PER_SECONDS as f64) as u128;
-
-#[derive(Copy, Clone, Debug)]
-struct SnakeStruct {
-    next_tile: Option<[usize; 2]>
-}
-
-#[derive(Copy, Clone, Debug)]
-enum TileState {
-    Food,
-    Snake(SnakeStruct),
-    Empty,
-}
-
-impl TileState {
-    pub fn get_color(&self) -> Color {
-        match self {
-            TileState::Food => [0.0, 100.0, 0.0, 1.0],
-            TileState::Snake(_) => [100.0, 0.0, 0.0, 1.0],
-            TileState::Empty => [0.0, 0.0, 0.0, 1.0],
-        }
-    }
-}
 
 #[derive(Debug)]
 enum MovementDirection {
@@ -67,7 +41,6 @@ struct World<const ROW_COUNT: usize, const COL_COUNT: usize> {
     pub row_count: usize,
     pub col_count: usize,
     pub rng: ThreadRng,
-    pub tiles: [[TileState; COL_COUNT]; ROW_COUNT],
     pub movement_direction: MovementDirection,
     pub snake_body: Vec<[usize; 2]>,
 }
@@ -79,7 +52,6 @@ impl<const ROW_COUNT: usize, const COL_COUNT: usize> World<ROW_COUNT, COL_COUNT>
             row_count: ROW_COUNT,
             col_count: COL_COUNT,
             rng: thread_rng(),
-            tiles: [[TileState::Empty; COL_COUNT]; ROW_COUNT],
             movement_direction: MovementDirection::Up,
             snake_body: Vec::new(),
         };
@@ -97,10 +69,8 @@ impl<const ROW_COUNT: usize, const COL_COUNT: usize> World<ROW_COUNT, COL_COUNT>
 
     pub fn step(&mut self) {
         if !self.is_running {
-            eprintln!("self.snake_body = {:#?}", self.snake_body);
             return;
         }
-
 
         let len = self.snake_body.len();
 
@@ -110,34 +80,21 @@ impl<const ROW_COUNT: usize, const COL_COUNT: usize> World<ROW_COUNT, COL_COUNT>
         }
 
         if let Some(last) = self.snake_body.last_mut() {
-            let mut temp_y: i32 = last[0] as i32;
-            let mut temp_x: i32 = last[1] as i32;
+            let wrap = |n: i32, max: u32| {
+                let max = max as i32;
+                if n >= 0 {
+                    n % max
+                } else {
+                    (max + (n % max)) % max
+                }
+            };
 
             match self.movement_direction {
-                MovementDirection::Up => { temp_y = last[0] as i32 - 1 }
-                MovementDirection::Left => { temp_x = last[1] as i32 - 1 }
-                MovementDirection::Down => { temp_y = last[0] as i32 + 1 }
-                MovementDirection::Right => { temp_x = last[1] as i32 + 1 }
+                MovementDirection::Up => { last[0] = wrap(last[0] as i32 - 1, self.row_count as u32) as usize }
+                MovementDirection::Left => { last[1] = wrap(last[1] as i32 - 1, self.col_count as u32) as usize }
+                MovementDirection::Down => { last[0] = wrap(last[0] as i32 + 1, self.row_count as u32) as usize }
+                MovementDirection::Right => { last[1] = wrap(last[1] as i32 + 1, self.col_count as u32) as usize }
             }
-
-            if temp_y < 0 {
-                temp_y = (self.row_count as i32 - 1);
-            }
-
-            if temp_y > (self.row_count as i32 - 1) {
-                temp_y = 0;
-            }
-
-            if temp_x < 0 {
-                temp_x = (self.col_count as i32 - 1);
-            }
-
-            if temp_x > (self.col_count as i32 - 1) {
-                temp_x = 0;
-            }
-
-            last[0] = temp_y as usize;
-            last[1] = temp_x as usize;
         }
     }
 }
@@ -180,12 +137,7 @@ fn main() {
 
         // This part of code ensures that the program always runs at the predetermined amount of FPS rate, e.g. 60
         if previous_update.elapsed().map(|d| d.as_millis()).unwrap_or(0) > MILLIS_PER_FRAME {
-            let step_start = SystemTime::now();
-
             world.step();
-
-
-            println!("Step took: {}ms", step_start.elapsed().map(|d| d.as_micros()).unwrap_or(0) as f32 / 1000.0);
             previous_update = SystemTime::now();
         }
 
@@ -196,8 +148,8 @@ fn main() {
             // CLEAR SCREEN
             clear([200.0, 120.0, 200.0, 0.5], graphics);
 
-            for (i_row, row) in world.tiles.iter().enumerate() {
-                for (i_col, tile) in row.iter().enumerate() {
+            for i_row in 0..world.row_count {
+                for i_col in 0..world.col_count {
                     let rectangle_def = [
                         i_col as f64 * TILE_SIZE,
                         i_row as f64 * TILE_SIZE,
@@ -205,7 +157,7 @@ fn main() {
                         (i_row + 1) as f64 * TILE_SIZE
                     ];
 
-                    let mut color = tile.get_color();
+                    let mut color = COLOR_EMPTY;
                     if world.snake_body.contains(&[i_row, i_col]) {
                         color = COLOR_SNAKE;
                     }
@@ -224,6 +176,7 @@ fn main() {
                             context.transform,
                             graphics,
                         )
+
                 }
             }
         });
